94367fc8d6366c0b90cf80be5e7ce9aa4c657e9d
Dionysios Kalofonos <peitemou@gmail.com>
2014-03-17 13:05:33 +0200

    Resolves an issue where the CTCoreMessageMemLeaks.patch was not applicable.

--- _subrepo-patches/apply.sh
+++ _subrepo-patches/apply.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ROOT=$(cd .. && pwd)
 echo $ROOT
-cd $ROOT          && git am --signoff <  $ROOT/_subrepo-patches/CTCoreMessageMemLeaks.patch
 cd $ROOT/iOSPorts && git am --signoff <  $ROOT/_subrepo-patches/iOSPorts-XCodeSettings.patch
 cd $ROOT/libetpan && git am --signoff <  $ROOT/_subrepo-patches/libetpan-XCodeSettings.patch
 cd $ROOT          && git am --signoff <  $ROOT/_subrepo-patches/MailCore-XCodeSettings-5.1.patch
+cd $ROOT          && git am --signoff <  $ROOT/_subrepo-patches/CTCoreMessageMemLeaks.patch
 #eof
