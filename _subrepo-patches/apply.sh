40d661819e2ab14719f835e27870cf05f33b040d
Dionysios Kalofonos <peitemou@gmail.com>
2014-03-17 09:53:30 +0200

    Introduces the MailCore-XCodeSettings-5.1.patch.

--- _subrepo-patches/apply.sh
+++ _subrepo-patches/apply.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd ../iOSPorts && git am --signoff <  ../_subrepo-patches/iOSPorts-XCodeSettings.patch
 cd ../libetpan && git am --signoff <  ../_subrepo-patches/libetpan-XCodeSettings.patch
+cd ../libetpan && git am --signoff <  ../_subrepo-patches/MailCore-XCodeSettings-5.1.patch
 #eof
